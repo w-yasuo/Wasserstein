114fabe5872a24ac30c1fb203215f6e388876a9d
w-yasuo <57907479+w-yasuo@users.noreply.github.com>
2021-12-21 17:55:44 +0800

    Add files via upload

--- loss.py
+++ loss.py
@@ -1,4 +1,5 @@
 import cv2
+import torch
 import numpy as np
 from torch import nn
 from scipy import *
@@ -18,17 +19,17 @@ from scipy import *
 
 
 class L2loss(nn.Module):
-    def __init__(self, use_gpu=False, gpu_device=None):
-        self.use_gpu = use_gpu
-        self.gpu_device = None
-        # if use_gpu and gpu_device is not None:
-        #     self.gpu_device = gpu_device
+
+    def __init__(self):
+        super(L2loss, self).__init__()
+        self.h1 = np.zeros((224 * 224))
+        self.h2 = np.zeros((224 * 224))
 
     def forward(self, target, output):
-        h1 = np.zeros((224 * 224))
-        h2 = np.zeros((224 * 224))
         loss = 0
         for i in range(3):
+            # self.h1 = np.zeros((224 * 224))
+            # self.h2 = np.zeros((224 * 224))
             # print("*****************************")
             # print("target.type:", type(target))
             # print("output.type:", type(output))
@@ -43,31 +44,66 @@ class L2loss(nn.Module):
 
             for j in range(len(target[i])):
                 if j == 0:
-                    h1[:int(target[i, j, 0])] = 0
-                    h2[:int(output[i, j, 0])] = 0
+                    self.h1[:int(target[i, j, 0])] = 0
+                    self.h2[:int(output[i, j, 0])] = 0
                 elif j == len(target[i]) - 1:
-                    h1[int(target[i, j, 0]):] = j
-                    h2[int(output[i, j, 0]):] = j
+                    self.h1[int(target[i, j, 0]):] = j
+                    self.h2[int(output[i, j, 0]):] = j
                 else:
-                    h1[int(target[i, j - 1, 0]):int(target[i, j, 0])] = j
-                    h2[int(output[i, j - 1, 0]):int(output[i, j, 0])] = j
+                    self.h1[int(target[i, j - 1, 0]):int(target[i, j, 0])] = j
+                    self.h2[int(output[i, j - 1, 0]):int(output[i, j, 0])] = j
 
-            loss += np.sqrt(np.sum(np.square(np.absolute(h1 - h2))))
+            loss += np.sqrt(np.sum(np.square(np.absolute(self.h1 - self.h2))))
+        print("loss:", loss)
         return loss
 
 
 if __name__ == '__main__':
-    img1 = cv2.imread(r"D:\TEST_IMAGE\3.jpg")
+    # img1 = cv2.imread(r"D:\TEST_IMAGE\3.jpg")
+    img1 = cv2.imread(r"D:\Datasets\yunse\train\crop_img_26_52.tif")
     hist1 = cv2.calcHist([img1], [0], None, [256], [0, 256])
     hist2 = cv2.calcHist([img1], [1], None, [256], [0, 256])
     hist3 = cv2.calcHist([img1], [2], None, [256], [0, 256])
     pred = np.concatenate((hist1, hist2, hist3))
     pred = pred.reshape((3, 256, 1))
 
-    img2 = cv2.imread(r"D:\TEST_IMAGE\3.jpg")
+    # img2 = cv2.imread(r"D:\TEST_IMAGE\3.jpg")
+    img2 = cv2.imread(r"D:\Datasets\yunse\train\crop_img_27_46.tif")
     hist2_1 = cv2.calcHist([img2], [0], None, [256], [0, 256])
     hist2_2 = cv2.calcHist([img2], [1], None, [256], [0, 256])
     hist2_3 = cv2.calcHist([img2], [2], None, [256], [0, 256])
     target = np.concatenate((hist2_1, hist2_2, hist2_3))
     target = target.reshape((3, 256, 1))
-    L2loss(pred, target)
+
+    loss = L2loss()(target, pred)
+    print(loss)
+
+    # loss = 0
+    # for i in range(3):
+    #     h1 = np.zeros((224 * 224))
+    #     h2 = np.zeros((224 * 224))
+    #     # print("*****************************")
+    #     # print("target.type:", type(target))
+    #     # print("output.type:", type(output))
+    #     # print("*****************************")
+    #     # print("target.shape:", target.shape)
+    #     # print("output.shape:", output.shape)
+    #     # print("*****************************")
+    #     # print("target[i].shape:", target[i].shape)
+    #     # print("output[i].shape:", output[i].shape)
+    #     target[i, :, 0] = np.cumsum(target[i])
+    #     pred[i, :, 0] = np.cumsum(pred[i])
+    #
+    #     for j in range(len(target[i])):
+    #         if j == 0:
+    #             h1[:int(target[i, j, 0])] = 0
+    #             h2[:int(pred[i, j, 0])] = 0
+    #         elif j == len(target[i]) - 1:
+    #             h1[int(target[i, j, 0]):] = j
+    #             h2[int(pred[i, j, 0]):] = j
+    #         else:
+    #             h1[int(target[i, j - 1, 0]):int(target[i, j, 0])] = j
+    #             h2[int(pred[i, j - 1, 0]):int(pred[i, j, 0])] = j
+    #
+    #     loss += np.sqrt(np.sum(np.square(np.absolute(h1 - h2))))
+    # print("loss:", loss)
